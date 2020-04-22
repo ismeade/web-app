f009483b941631cbd017a2da6b9141efaa46ba4e
liyang <ismeade99@sina.com>
2020-04-22 12:35:26 +0800

    create

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/zserge/webview"
+)
+
+var web webview.WebView
+var appSlice []string
+
+var (
+	h bool
+	v bool
+	l bool
+)
+
+func main() {
+	initAppSlice()
+	initFlag()
+
+	parseArgs()
+
+	if web != nil {
+		web.Run()
+	}
+}
+
+func initAppSlice() {
+	appSlice = append(appSlice, "wechat", "youdao")
+}
+
+func initFlag() {
+	flag.BoolVar(&h, "h", false, "help")
+	flag.BoolVar(&v, "v", false, "version")
+	flag.BoolVar(&l, "l", false, "support app")
+	flag.Parse()
+	flag.Usage = usage
+}
+
+func parseArgs() {
+	if h {
+		flag.Usage()
+		return
+	}
+
+	if v {
+		fmt.Println("0.0.1")
+		return
+	}
+
+	if l {
+		fmt.Println("support:")
+		for _, value := range appSlice {
+			fmt.Printf("\t%s\n", value)
+		}
+		return
+	}
+
+	if flag.NArg() == 0 {
+		usage()
+		return
+	}
+
+	apps := flag.Args()
+	if len(apps) > 0 {
+		switch apps[0] {
+		case "wechat", "wx":
+			web = makeWebview("wechat", 1000, 800,
+				"https://wx.qq.com/", false)
+		case "youdao", "yd":
+			web = makeWebview("youdao", 1000, 800,
+				"https://note.youdao.com/web/", true)
+		default:
+			fmt.Println("No support:", apps[0])
+		}
+	}
+}
+
+func makeWebview(title string, wide int, high int,
+	url string, chenge bool) webview.WebView {
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle(title)
+	if chenge {
+		w.SetSize(wide, high, webview.HintNone)
+	} else {
+		w.SetSize(wide, high, webview.HintFixed)
+	}
+	w.Navigate(url)
+	return w
+}
+
+func usage() {
+	fmt.Println(`example:
+    web-app wechat`)
+	flag.PrintDefaults()
+}
