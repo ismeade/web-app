b07a3952f4ae946213c820b97a94fe6eb11c3d57
liyang <ismeade99@sina.com>
2021-01-06 01:22:34 +0800

    up

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ismeade/web-app
 
 go 1.14
 
-require github.com/webview/webview v0.0.0-20200724072439-e0c01595b361
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/webview/webview v0.0.0-20200724072439-e0c01595b361
+)
--- web/serv.go
+++ web/serv.go
@@ -1,28 +1,36 @@
 package web
 
 import (
-	"fmt"
-	"io"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		io.WriteString(w, "<html><head></head><body><h1>Welcome!</h1><img src=\"/static/test.jpg\"></body></html>")
-		return
-	}
-}
+//func hello(w http.ResponseWriter, r *http.Request) {
+//if r.Method == "GET" {
+//io.WriteString(w, "<html><head></head><body><h1>Welcome!</h1><img src=\"/static/IMG_0547.JPG\"></body></html>")
+//return
+//}
+//}
 
-func static(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, r.URL.Path[1:])
+func homeFunc(c *gin.Context) {
+	c.String(http.StatusOK, "<html><head></head><body><h1>Welcome!</h1><img src=\"/static/IMG_0547.JPG\"></body></html>", nil)
 }
 
 func StartUp() {
-	http.Handle("/static/", http.FileServer(http.Dir("static")))
-	//http.HandleFunc("/static/", static)
-	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Listen And Server", err.Error())
-	}
+	gin.SetMode(gin.ReleaseMode)
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+	r.Static("/static", "./static")
+	// GET：请求方式；/hello：请求的路径
+	// 当客户端以GET方法请求/hello路径时，会执行后面的匿名函数
+	r.GET("/", func(c *gin.Context) {
+		// c.JSON：返回JSON格式的数据
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello world!",
+		})
+	})
+	r.GET("/home", homeFunc)
+	// 启动HTTP服务，默认在0.0.0.0:8080启动服务
+	r.Run(":8080")
 }
